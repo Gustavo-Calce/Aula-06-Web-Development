a109a1b7103a1ffe0379b83d2fb06e1f5c25960d
Gustavo Calce <gustavo.calcesilva@gmail.com>
2023-04-04 10:53:48 -0300

    Aula 06

---
+++ script.js
@@ -0,0 +1,14 @@
+const opcao = parseInt(prompt("Digite uma opção: "));
+switch(opcao){
+    case 1:
+        document.write("Primeira opção")
+        break;
+    case 2:
+        document.write("Segunda opção")
+        break;
+    case 3:
+        document.write("Terceira opção")
+        break;
+    default:
+        document.write("Nenhuma opção")
+}
