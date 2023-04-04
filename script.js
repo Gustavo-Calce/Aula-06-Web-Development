4b230241441f01adf638f330869c42c5fe7abc20
Gustavo Calce <gustavo.calcesilva@gmail.com>
2023-04-04 11:21:45 -0300

    Exercício de aula

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const opcao = parseInt(prompt("Digite uma opção: "));
 switch(opcao){
     case 1:
@@ -11,4 +12,31 @@ switch(opcao){
         break;
     default:
         document.write("Nenhuma opção")
+}
+
+let valor = 7;
+document.write("<br><br>")
+document.write(valor);
+document.write("<br><br>")
+valor = valor+1
+document.write(valor);
+document.write("<br><br>")
+
+let valor2 = 10;
+document.write(valor2)
+document.write("<br><br>")
+valor2 = valor2 -1
+document.write(valor2)
+document.write("<br><br>")
+
+
+for(let i=0; i<= 10; i++){
+    document.write("Agora i vale: "+i)
+    document.write("<br><br>")
+}
+
+let carros =["Celta ","Fit ","City ","H-RV ","Civic"]
+for(let i =0; i <carros.length;i++){
+    document.write(carros[i]);
+    document.write("<br><br>")
 }
